65c9c4f5d639be3c03e65cfe824f857347537552
Yanjie-SHI <amyshi2016@gmail.com>
2021-03-04 23:05:33 +0800

    feat:仅保留简单Ioc容器

--- run.php
+++ run.php
@@ -7,7 +7,9 @@ use Core\Ioc;
 
 //实例化IoC容器
 $ioc = new Ioc();
-$ioc->bind('App\MyInterface\Log', function(){return new FileLog();});
+#通过路径和闭包bind
+$ioc->bind('App\MyInterface\Log', function($ioc){return new FileLog();});
+#通过别名和路径bind
 $ioc->bind('User', "App\User");
 $user = $ioc->make('User');
 $user->login();
